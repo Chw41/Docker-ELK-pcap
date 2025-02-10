6f7b8a1b423f0b33c249b37ddb12916cabd9ca01
CHW <141213003+Chw41@users.noreply.github.com>
2025-02-10 21:28:11 +0800

    Update catch_pcap.py

--- catch_pcap.py
+++ catch_pcap.py
@@ -2,12 +2,12 @@ import os
 import requests
 import time
 
-url_template = 'http://35.221.181.38:8888/pcap/3/{}'
+url_template = '{your_pcap_target_url}/pcap/{TeamID}/{}'  # Replace with the actual target URL
 headers = {
     'accept': 'application/json',
-    'Authorization': '6b7ee05971955ed5359e93f53e46a999'
+    # Replace with a valid authorization token if needed
 }
-save_path = '/Users/CWei/Desktop/CyberSec/AIS3 EOF/2025/final/pcap'
+save_path = './pcap'
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
@@ -29,6 +29,6 @@ for round_number in range(90, 180):
             f.write(response.content)
         print(f'Catch Round: {round_number} Success')
     else:
-        print(f'Round: {round_number} Fail，狀態：{response.status_code}')
+        print(f'Round: {round_number} Fail，Status Code：{response.status_code}')
     
     time.sleep(3)
